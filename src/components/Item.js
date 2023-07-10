bdb60bfb564e72baecbc955983d07435152bc7a6
philip dutra <monsterphilip10@gmail.com>
2023-07-09 20:20:40 -0400

    updated README.md

--- src/components/Item.js
+++ src/components/Item.js
@@ -13,7 +13,7 @@ function Item({i, handleDelete}){
             <Card.Body>
                 <Card.Title>{i.name}</Card.Title>
                 <Card.Img variant="top" src={i.image} alt={i.name} style={{height: "15vw", width:"100%", objectFit:"contain", backgroundColor:"white" }}/>
-                <Card.Text>Size:{i.size}</Card.Text>
+                <Card.Text>Size: {i.size}</Card.Text>
                 <Card.Text >${i.price}</Card.Text>
                 <Button style={{backgroundColor: "red"}} onClick={handleClick}>Delete</Button> 
             </Card.Body>
